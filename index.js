3392d2fd704f79e6c748f9632288df1bb5bed688
pacosoares <services.paco@gmail.com>
2024-12-05 11:40:49 -0300

    Find datas of db test

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    //Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // database and collection code goes here
+    const db = client.db("sample_restaurants");
+    const coll = db.collection("restaurants");
+    // find code goes here
+    const cursor = coll.find({ cuisine: "American" });
+    // iterate code goes here
+    await cursor.forEach(console.log);
 
   } finally {
     // Ensures that the client will close when you finish/error
