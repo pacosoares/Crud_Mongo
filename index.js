3dedcac17fee8ebfe9d667f021f70e971852712a
pacosoares <services.paco@gmail.com>
2024-12-05 22:59:16 -0300

    Insert datas

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // database and collection code goes here
-    const coll = client.db("sample_guides").collection("planets");
-    // find code using AND < 15 and >-100
-    const cursor = coll.find({
-      $and: [{ orderFromSun: { $gt: 2 } }, { orderFromSun: { $lt: 5 } }],
-    });
-    // iterate code goes here
-    await cursor.forEach(console.log);
+    const coll = client.db("sample_guides").collection("comets");
+    // insert 
+    const docs = [
+      {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
+      {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
+      {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
+    ];
+    const result = await coll.insertMany(docs);
+    // display the results of your operation
+    console.log(result.insertedIds);
+
 
   } finally {
     // Ensures that the client will close when you finish/error
